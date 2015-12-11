a17e19ed90d4946e88380e2e132ccfd1df93d892
Vikram Jadhav <vikramcse.10@gmail.com>
2015-12-11 16:17:55 +0000

    Added assets, created course model and added templates

---
+++ courses/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Course(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+
+	def __str__(self):
+		return self.title
---
+++ courses/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Course
+
+def course_list(request):
+	courses = Course.objects.all()
+	return render(request, 'courses/course_list.html', {'courses': courses})
